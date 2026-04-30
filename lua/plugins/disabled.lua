6b6a55bd875885eec6022c37af7291dade81a197
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-04-30 15:04:21 +0200

    disable some unused plugins

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,8 @@
+return {
+  -- { "snacks.nvim", enabled = false },
+  { "tokyonight.nvim", enabled = false },
+  -- { "mini.ai", enabled = false },
+  { "nvim-lint", enabled = false },
+  { "nvim-ts-autotag", enabled = false },
+  { "ts-comments.nvim", enabled = false },
+}
