daaaba8a1d351843cb01bca4191ba608a5a0246b
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-04-30 15:42:24 +0200

    add extras to config/lazy

--- lua/plugins/extras.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { import = "lazyvim.plugins.extras.ui.mini-starter" },
-  { import = "lazyvim.plugins.extras.lang.go" },
-  { import = "lazyvim.plugins.extras.lang.docker" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
-  { import = "lazyvim.plugins.extras.lang.yaml" },
-}
