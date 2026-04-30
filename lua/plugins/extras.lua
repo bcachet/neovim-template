35929c3c3cfc77678de6a0b917b2c5585975e959
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-04-30 15:36:31 +0200

    add extras.lang.{json,yaml,markdown}

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,3 +1,8 @@
 return {
+  { import = "lazyvim.plugins.extras.ui.mini-starter" },
   { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
 }
