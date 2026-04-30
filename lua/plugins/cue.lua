163b6191d6cc30a347ff6ee915f632819fb08177
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-04-30 22:04:18 +0200

    cue: load LSP only when cue binary available'

--- lua/plugins/cue.lua
+++ lua/plugins/cue.lua
@@ -9,17 +9,15 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = true,
+    ft = { "cue" },
     opts = {
       servers = {
-        cue = {
-          cmd = {
-            "cue",
-            "lsp",
-            "serve",
-          },
+        cue = vim.fn.executable("cue") == 1 and {
+          cmd = { "cue", "lsp", "serve" },
           filetypes = { "cue" },
           single_file_support = true,
-        },
+        } or {},
       },
     },
   },
