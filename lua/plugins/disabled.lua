5a05dfe3692a61a0d85c29fea8940d9c10a28782
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-05-01 15:09:50 +0200

    Stop disabling plugins that are lazy loaded

--- lua/plugins/disabled.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- { "snacks.nvim", enabled = false },
-  { "tokyonight.nvim", enabled = false },
-  -- { "mini.ai", enabled = false },
-  { "nvim-lint", enabled = false },
-  { "nvim-ts-autotag", enabled = false },
-  { "ts-comments.nvim", enabled = false },
-}
