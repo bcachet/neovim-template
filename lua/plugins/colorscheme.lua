c4800326f97e9ab7896ed68d18a6c0e9e93cff23
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-05-01 15:07:09 +0200

    colors: colorscheme dark/light switchting

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,15 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-frappe",
-    },
+  "f-person/auto-dark-mode.nvim",
+  opts = {
+    set_dark_mode = function()
+      vim.opt.background = "dark"
+      vim.cmd.colorscheme("catppuccin-frappe")
+    end,
+    set_light_mode = function()
+      vim.opt.background = "light"
+      vim.cmd.colorscheme("catppuccin-latte")
+    end,
+    update_interval = 3000,
+    fallback = "dark",
   },
 }
