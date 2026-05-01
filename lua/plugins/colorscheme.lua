d8b60bd4c91e8bbd4f7e08d66144cea076c9ad80
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-05-01 14:56:10 +0200

    colors: Catppuccin Latte when system colorscheme is `prefer-light`

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,18 @@
 return {
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-frappe",
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "catppuccin-frappe",
+      },
+    },
+
+    {
+      "cosmicboots/system-theme.nvim",
+      config = {
+        dark_theme = "catppuccin-frappe",
+        light_theme = "catppuccin-latte",
+      },
     },
   },
 }
