4b71f174cb6fa7b731ece9475d351a5a97bb81e3
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-05-01 15:00:10 +0200

    Revert "colors: Catppuccin Latte when system colorscheme is `prefer-light`"
    This reverts commit a4dd0aced495b2562857991ed20d0ee539505661.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,8 @@
 return {
   {
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "catppuccin-frappe",
-      },
-    },
-
-    {
-      "cosmicboots/system-theme.nvim",
-      config = {
-        dark_theme = "catppuccin-frappe",
-        light_theme = "catppuccin-latte",
-      },
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
     },
   },
 }
