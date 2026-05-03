bcc4b63f27962eb65abcb5069d5864859d70613f
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-05-03 21:42:22 +0200

    add zk-nvim

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -1,13 +1,4 @@
 return {
-  -- {
-  --   "jakewvincent/mkdnflow.nvim",
-  --   ft = { "markdown", "rmd" }, -- Add custom filetypes here if configured
-  --   config = function()
-  --     require("mkdnflow").setup({
-  --       -- Your config
-  --     })
-  --   end,
-  -- },
   {
     "renerocksai/telekasten.nvim",
     dependencies = {
@@ -24,6 +15,28 @@ return {
       { "<leader>zd", "<cmd>Telekasten goto_today<CR>", desc = "Telekasten today", mode = { "n" } },
       { "<leader>zz", "<cmd>Telekasten follow_link<CR>", desc = "Telekasten follow link", mode = { "n" } },
       { "<leader>zn", "<cmd>Telekasten new_templated_note<CR>", desc = "Telekasten new note", mode = { "n" } },
+    },
+  },
+  {
+    "zk-org/zk-nvim",
+    name = "zk",
+    opts = {
+      -- Can be "telescope", "fzf", "fzf_lua", "minipick", "snacks_picker",
+      -- or select" (`vim.ui.select`).
+      picker = "telescope",
+
+      lsp = {
+        -- `config` is passed to `vim.lsp.start(config)`
+        config = {
+          name = "zk",
+          cmd = { "zk", "lsp" },
+          filetypes = { "markdown" },
+        },
+
+        auto_attach = {
+          enabled = true,
+        },
+      },
     },
   },
 }
