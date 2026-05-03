17fbba2930bd4ffd43bea6cb3846dd5c941b25c7
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-05-03 21:20:27 +0200

    telekasten: more keymaps

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -20,6 +20,10 @@ return {
     },
     keys = {
       { "<leader>z", "<cmd>Telekasten panel<CR>", desc = "Telekasten panel", mode = { "n" } },
+      { "<leader>zf", "<cmd>Telekasten find_notes<CR>", desc = "Telekasten find note", mode = { "n" } },
+      { "<leader>zd", "<cmd>Telekasten goto_today<CR>", desc = "Telekasten today", mode = { "n" } },
+      { "<leader>zz", "<cmd>Telekasten follow_link<CR>", desc = "Telekasten follow link", mode = { "n" } },
+      { "<leader>zn", "<cmd>Telekasten new_templated_note<CR>", desc = "Telekasten new note", mode = { "n" } },
     },
   },
 }
