79df626c858a17d977451a1159fa6f5ca83c4f50
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-05-03 20:46:25 +0200

    s/mkdnflow/telekasten

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -1,11 +1,22 @@
 return {
+  -- {
+  --   "jakewvincent/mkdnflow.nvim",
+  --   ft = { "markdown", "rmd" }, -- Add custom filetypes here if configured
+  --   config = function()
+  --     require("mkdnflow").setup({
+  --       -- Your config
+  --     })
+  --   end,
+  -- },
   {
-    "jakewvincent/mkdnflow.nvim",
-    ft = { "markdown", "rmd" }, -- Add custom filetypes here if configured
-    config = function()
-      require("mkdnflow").setup({
-        -- Your config
-      })
-    end,
+    "renerocksai/telekasten.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "telescope-media-files.nvim",
+      "calendar-vim",
+    },
+    opts = {
+      home = vim.fn.expand("~/Documents/Notes/"),
+    },
   },
 }
