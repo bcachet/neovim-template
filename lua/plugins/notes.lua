1c50c1c439fad218a26e93fbf44fbfe3ac0df6e0
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-05-03 22:15:15 +0200

    remove telekasten

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -1,28 +1,11 @@
 return {
   {
-    "renerocksai/telekasten.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "nvim-telescope/telescope-media-files.nvim",
-      "mattn/calendar-vim",
-    },
-    opts = {
-      home = vim.fn.expand("~/Documents/Notes/"),
-    },
-    keys = {
-      { "<leader>z", "<cmd>Telekasten panel<CR>", desc = "Telekasten panel", mode = { "n" } },
-      { "<leader>zf", "<cmd>Telekasten find_notes<CR>", desc = "Telekasten find note", mode = { "n" } },
-      { "<leader>zd", "<cmd>Telekasten goto_today<CR>", desc = "Telekasten today", mode = { "n" } },
-      { "<leader>zz", "<cmd>Telekasten follow_link<CR>", desc = "Telekasten follow link", mode = { "n" } },
-      { "<leader>zn", "<cmd>Telekasten new_templated_note<CR>", desc = "Telekasten new note", mode = { "n" } },
-    },
-  },
-  {
     "zk-org/zk-nvim",
     name = "zk",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
     opts = {
-      -- Can be "telescope", "fzf", "fzf_lua", "minipick", "snacks_picker",
-      -- or select" (`vim.ui.select`).
       picker = "telescope",
 
       lsp = {
