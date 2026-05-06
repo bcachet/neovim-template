29e2b5db54fdc171a136bafddb204ac06a1abc40
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-05-06 08:31:39 +0200

    fixup! Configure blink.cmp to complete only on <Tab>

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,36 +1,11 @@
-local has_words_before = function()
-  local col = vim.api.nvim_win_get_cursor(0)[2]
-  if col == 0 then
-    return false
-  end
-  local line = vim.api.nvim_get_current_line()
-  return line:sub(col, col):match("%s") == nil
-end
-
 return {
   {
     "blink.cmp",
     opts = {
-      keymap = {
-        preset = "none",
-        -- If completion hasn't been triggered yet, insert the first suggestion; if it has, cycle to the next suggestion.
-        ["<Tab>"] = {
-          function(cmp)
-            if has_words_before() then
-              return cmp.insert_next()
-            end
-          end,
-          "fallback",
-        },
-        -- Navigate to the previous suggestion or cancel completion if currently on the first one.
-        ["<S-Tab>"] = { "insert_prev" },
-      },
       completion = {
         menu = {
           auto_show = false,
-          auto_show_delay_ms = 500,
         },
-        list = { selection = { preselect = false }, cycle = { from_top = false } },
       },
     },
   },
