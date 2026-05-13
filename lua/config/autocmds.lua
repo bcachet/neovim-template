2ccf5ab8e7356c49da55dfe30c9e6a6cc64c617f
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-05-13 12:07:07 +0200

    fixup! autoformat all files except clojure

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,9 +8,9 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 --
 vim.api.nvim_create_autocmd({"FileType"}, {
-  pattern = { "cue" },
+  pattern = { "clojure" },
   callback = function()
-    vim.b.autoformat = true
+    vim.b.autoformat = false
   end
 })
 
