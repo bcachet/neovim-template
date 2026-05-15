55b3e6a3fef40cb5bff31aba249535b4bbf4eb25
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-05-15 18:17:40 +0200

    fixup! disable snippet

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,21 +1,47 @@
--- Configuration to disable automatic snippet triggering
+-- Configuration to disable automatic snippet and completion triggering
 return {
-  -- Override nvim-cmp configuration to disable snippet completion
+  -- Override nvim-cmp configuration to control completion behavior
   {
     "hrsh7th/nvim-cmp",
     optional = true,
     opts = function(_, opts)
-      -- Remove snippet sources from cmp
+      -- Disable automatic completion while typing
+      if opts.completion then
+        opts.completion.autocomplete = false  -- Disable auto-completion trigger
+      end
+
+      -- Configure completion to only show when manually triggered
+      if opts.completion then
+        opts.completion.completeopt = "menu,menuone,noselect"  -- Standard completion options
+      end
+
+      -- Remove snippet sources from cmp to prevent snippet-based completions
       if opts.sources then
         opts.sources = vim.tbl_filter(function(source)
           return not (source.name == "luasnip" or source.name == "snippy")
         end, opts.sources)
       end
 
-      -- Disable snippet completion in mapping
+      -- Configure mappings to only trigger completion on TAB
       if opts.mapping then
-        opts.mapping["<Tab>"] = nil
-        opts.mapping["<S-Tab>"] = nil
+        opts.mapping = {
+          ["<Tab>"] = function(fallback)
+            if require("cmp").visible() then
+              require("cmp").select_next_item()
+            else
+              require("cmp").complete()  -- Manually trigger completion
+            end
+          end,
+          ["<S-Tab>"] = function(fallback)
+            if require("cmp").visible() then
+              require("cmp").select_prev_item()
+            else
+              fallback()
+            end
+          end,
+          ["<CR>"] = require("cmp").mapping.confirm({ select = true }),  -- Confirm selection
+          ["<C-e>"] = require("cmp").mapping.abort(),  -- Abort completion
+        }
       end
     end,
   },
