2218baa904c043dc62a32f7ff5bc778d200280ff
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-05-16 11:27:53 +0200

    no autoformat for chezmoi (break template)

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,10 +7,15 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 --
-vim.api.nvim_create_autocmd({"FileType"}, {
+vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "clojure" },
   callback = function()
     vim.b.autoformat = false
-  end
+  end,
 })
-
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "chezmoi" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
