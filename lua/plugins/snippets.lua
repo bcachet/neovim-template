2af55edcfb3207d8a149321d9ca8301aba573f1d
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-05-16 11:24:39 +0200

    snippet: disable friendly-snippets

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -7,12 +7,12 @@ return {
     opts = function(_, opts)
       -- Disable automatic completion while typing
       if opts.completion then
-        opts.completion.autocomplete = false  -- Disable auto-completion trigger
+        opts.completion.autocomplete = false -- Disable auto-completion trigger
       end
 
       -- Configure completion to only show when manually triggered
       if opts.completion then
-        opts.completion.completeopt = "menu,menuone,noselect"  -- Standard completion options
+        opts.completion.completeopt = "menu,menuone,noselect" -- Standard completion options
       end
 
       -- Remove snippet sources from cmp to prevent snippet-based completions
@@ -29,7 +29,7 @@ return {
             if require("cmp").visible() then
               require("cmp").select_next_item()
             else
-              require("cmp").complete()  -- Manually trigger completion
+              require("cmp").complete() -- Manually trigger completion
             end
           end,
           ["<S-Tab>"] = function(fallback)
@@ -39,8 +39,8 @@ return {
               fallback()
             end
           end,
-          ["<CR>"] = require("cmp").mapping.confirm({ select = true }),  -- Confirm selection
-          ["<C-e>"] = require("cmp").mapping.abort(),  -- Abort completion
+          ["<CR>"] = require("cmp").mapping.confirm({ select = true }), -- Confirm selection
+          ["<C-e>"] = require("cmp").mapping.abort(), -- Abort completion
         }
       end
     end,
@@ -51,19 +51,12 @@ return {
     "L3MON4D3/LuaSnip",
     optional = true,
     opts = function(_, opts)
-      -- Disable auto-triggering of snippets
-      if opts.enable_autosnippets == nil then
-        opts.enable_autosnippets = false
-      end
-
-      -- You can also disable specific snippet types
-      -- opts.ft_func = function() return {} end -- Disable filetype-specific snippets
+      opts.enable_autosnippets = false
     end,
   },
 
-  -- Optionally disable friendly-snippets if you don't want any snippets at all
-  -- {
-  --   "rafamadriz/friendly-snippets",
-  --   enabled = false,
-  -- },
+  {
+    "rafamadriz/friendly-snippets",
+    enabled = false,
+  },
 }
