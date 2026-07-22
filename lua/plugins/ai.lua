42c7c31bfc9d9d521ef6ec3fbe97bfcd4c707f84
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2026-07-22 09:53:13 +0200

    codecomponaion: configure poolside

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -2,10 +2,27 @@ return {
   {
     "olimorris/codecompanion.nvim",
     version = "^19.0.0",
-    opts = {},
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      strategies = {
+        chat = { adapter = "poolside" },
+        inline = { adapter = "poolside" },
+      },
+      adapters = {
+        poolside = function()
+          return {
+            name = "poolside",
+            formatted_name = "Poolside",
+            type = "acp",
+            commands = {
+              default = { "pool", "acp" },
+            },
+          }
+        end,
+      },
     },
   },
 }
